c4f2e601f3ffe22057992dd79658b2a715c49400
007matan <1234matan@gmail.com>
2023-07-10 19:08:30 +0300

    Continue HomePage UI

---
+++ PawPartner/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  PawPartner
+//
+//  Created by Student11 on 10/07/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
